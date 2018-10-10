8db338685ea1163b2f6ac42155404ea8dfb9dc7f
Arjun Dhiman <arjund38@gmail.com>
2018-10-10 16:32:42 -0400

    added content to index.php

--- index.php
+++ index.php
@@ -1,3 +1,55 @@
-<?php
-echo "In progress";
-?>
+<!doctype html>
+<html lang="en">
+<title>Arjun Dhiman</title>
+<meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<body>
+  <div class="container">
+<header>
+  <h1 class="display-3"> HI! My name is Arjun Dhiman </h1>
+</header><br/>
+<!-- <nav>
+  <ul>
+    <li><a href=#> My Projects</a></li>
+  </ul>
+</nav> -->
+<a href="mailto:arjund38@gmail.com">Email</a>  &nbsp; | &nbsp;
+<a href="https://github.com/arjundhiman786/">GitHub</a>  &nbsp; |  &nbsp;
+<a href="other/resume.pdf">Resume</a>
+<div id="projects">
+<h1 class="display-4"> My Projects </h1>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <div class="card">
+        <!--<img class="card-img-top" src="..." alt="Card image cap">-->
+        <div class="card-body">
+          <h5 class="card-title">Diary</h5>
+          <p class="card-text">
+            An android application that allows an individual to write about whatever they want in public,or private.</p>
+            <a href="https://github.com/arjundhiman786/diary" class="btn btn-primary">View of GitHub</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm">
+      <div class="card">
+        <!--<img class="card-img-top" src="..." alt="Card image cap">-->
+        <div class="card-body">
+          <h5 class="card-title">Personal website</h5>
+          <p class="card-text">
+            The repository that contains all of the code and all the material used to create this website.</p>
+            <a href="https://github.com/arjundhiman786/personal_website" class="btn btn-primary">View of GitHub</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<footer>&copy; 2018 - present Arjun Dhiman </footer>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
