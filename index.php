1effa1d3cecf8f077cde6df3ae6afe360502da86
Arjun Dhiman <arjund38@gmail.com>
2018-10-28 00:38:42 -0400

    Made new changes to website. Added About me section, and icons for social media links

--- index.php
+++ index.php
@@ -4,22 +4,51 @@
 <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+<link rel="stylesheet" href="css/main.css">
 <body>
   <div class="container">
 <header>
-  <h1 class="display-3"> HI! My name is Arjun Dhiman </h1>
-</header><br/>
-<!-- <nav>
-  <ul>
-    <li><a href=#> My Projects</a></li>
-  </ul>
-</nav> -->
-<a href="mailto:arjund38@gmail.com">Email</a>  &nbsp; | &nbsp;
-<a href="https://github.com/arjundhiman786/">GitHub</a>  &nbsp; |  &nbsp;
-<a href="https://www.linkedin.com/in/arjun-dhiman-7ab404110/">Linkedin</a>  &nbsp; |  &nbsp;
-<a href="other/resume.pdf">Resume</a>
+<div id="social-media-links">
+  <a href="mailto:arjund38@gmail.com"><i class="fas fa-envelope fa-2x"></i></a>
+  <a href="https://github.com/arjundhiman786/"><i class="fab fa-github fa-2x"></i></a>
+  <a href="https://www.linkedin.com/in/arjun-dhiman-7ab404110/"><i class="fab fa-linkedin fa-2x"></i></a>
+</div>
+  <h1 class="display-3">Arjun Dhiman</h1>
+</header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="other/resume.pdf">Resume</a>
+      </li>
+      <!--<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li> -->
+    </ul>
+  </div>
+</nav>
+<div id="aboutme">
+  <h2 class="display-4">About me</h2>
+  <p>HI! my name is Arjun Dhiman, and this is my personal website.
+    I am currently an undergraduate student at the University of Toronto Mississauga where
+    I am currently studying Computer Science and Mathematics. On this website, you can find my most
+    recently updated resume, and the project I am currently or have previously worked on.
+  </p>
+</div>
 <div id="projects">
-<h1 class="display-4"> My Projects </h1>
+<h1 class="display-4">My Projects</h1>
 <div class="container">
   <div class="row">
     <div class="col-sm">
@@ -47,7 +76,7 @@
   </div>
 </div>
 </div>
-<footer>&copy; 2018 - Present Arjun Dhiman </footer>
+<footer style="text-align:center">&copy; 2018 - Present Arjun Dhiman </footer>
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
